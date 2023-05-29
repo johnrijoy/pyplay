cdc0145c3d50e00e30955fed5aef92a73aabc030
Rijoy John <johnrijoy@gmail.com>
2023-05-30 00:53:06 +0530

    added rich progress for now playing

--- src/frontend_cli.py
+++ src/frontend_cli.py
@@ -1,13 +1,20 @@
 from player import PlaybackController
+from utils import AudioDetails, AudioState
 from fetcher import search_song, get_song
+from rich import print as rprint
+from rich.progress import (
+    BarColumn,
+    Progress,
+    TextColumn,
+)
 
 def welcome_screen():
-    print("--------------- PYPLAY ----------------------")
-    print()
-    print("Welcome to Pyplay.\nEnter HELP for list of commands")
-    print("\nplay song_name \n")
-    print("to start playing a song")
-    print("---------------------------------------------")
+    rprint("--------------- [bold green]PYPLAY[/bold green] ----------------------")
+    rprint()
+    rprint("Welcome to Pyplay.\nEnter HELP for list of commands")
+    rprint("\nplay song_name \n")
+    rprint("to start playing a song")
+    rprint("---------------------------------------------")
 
 def help_screen():
     print("play <song_name>   - play song ")
@@ -22,6 +29,27 @@ def help_screen():
     print("setvol <0-100>     - enter value between 0 and 100 to set volume")
     print("end , q            - to exit pyplay")
 
+def show_now_playing(audioState : AudioState):
+
+    curr_time = "{:02d}:{:02d}".format(audioState.current_position//60, (audioState.current_position)%60)
+    tot_time = "{:02d}:{:02d}".format(audioState.total_duration//60, (audioState.total_duration)%60)
+
+
+    # Define custom progress bar
+    progress_bar = Progress(
+        TextColumn("[green] ▶ "),
+        BarColumn(),
+        TextColumn("[green]{}".format(curr_time)),
+        TextColumn("•"),
+        TextColumn("[cyan]{}".format(tot_time)),
+    )
+
+    # Use custom progress bar
+    with progress_bar as p:
+        p.console.print("[green]Now Playing")
+        p.console.print("[bold red]%40s[/bold red] - [orange1]%s[/orange1]" % (audioState.title[:40], audioState.uploader))
+        p.add_task("", completed= audioState.current_position, total=audioState.total_duration)
+
 def process_input(command: str, args: list, playbackController: PlaybackController):
 
     command = command.lower()
@@ -33,27 +61,33 @@ def process_input(command: str, args: list, playbackController: PlaybackControll
             audioDetails = get_song(" ".join(args))
             playbackController.append_song(audioDetails)
         playbackController.start_playback()
+
     elif command == 'add' or command == 'a':
         if(len(args)<1):
             print("Atleast one argument required")
         else:
             audioDetails = get_song(" ".join(args))
             playbackController.append_song(audioDetails)
+
     elif command == 'pause' or command == 'resume' or command == 'p':
         playbackController.pause_resume()
+
     elif command == 'skip' or command == 'sk':
         playbackController.skip_song()
+
     elif command == 'showq' or command == 'sq':
         song_queue = playbackController.songQ
         print("-- Playlist --")
         for index, song in enumerate(song_queue):
             print("{:3} - {:20} - {}".format(index+1, song.title, song.get_duration()))
+
     elif command == 'nowp' or command == 'n':
         audio_state = playbackController.get_now_playing()
-        print("-- Now Playing --")
-        print("{}".format(audio_state))
+        show_now_playing(audio_state)
+
     elif command == 'rmlast' or command == 'rml':
         playbackController.remove_last()
+
     elif command == 'search' or command == 'sr':
         if(len(args)<1):
             print("Atleast one argument required")
@@ -91,8 +125,10 @@ def process_input(command: str, args: list, playbackController: PlaybackControll
 
     elif command == 'stop' or command == 'st':
         playbackController.stop_playback()
+
     elif command == 'help' or command == 'h':
         help_screen()
+
     else:
         print("Enter a valid command. Type HELP for list of available commands")
 
